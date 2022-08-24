65e922bf6b0a3d39e6f8f74cef8ce63a84d69541
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-08-24 15:35:02 +0300

    create repos

--- RestWebApi/Repositories/Implementations/Repository.cs
+++ RestWebApi/Repositories/Implementations/Repository.cs
@@ -1,5 +1,6 @@
 using Models.Implementations;
 using Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repositories.Implementations
 {
@@ -10,9 +11,82 @@ namespace Repositories.Implementations
         {
             _context = context;
         }
-        public List<Order> GetOrders()
+
+        public async Task<List<Order>> GetOrders()
         {
-            return _context.Orders.ToList();
+            try
+            {
+                return await _context.Orders.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public async Task<Order> GetOrderById(int orderNumber)
+        {
+            try
+            {
+                return await _context.Orders.FirstOrDefaultAsync(o => o.OrderNumber == orderNumber);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            
+        }
+
+        public async Task<bool> CreateOrder(Order orderToCreate)
+        {
+            try
+            {
+                await _context.Orders.AddAsync(orderToCreate);
+                return await _context.SaveChangesAsync() >= 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteOrder(int orderNumberToDelete)
+        {
+            try
+            {
+                Order findedOrder = GetOrderById(orderNumberToDelete);
+
+                if(findedOrder != null)
+                {
+                    _context.Orders.Remove(findedOrder);
+                    _context.SaveChangesAsync();
+                    return true;
+                }
+                else return false;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateOrder(Order orderToUpdate)
+        {
+            try
+            {
+                _context.Orders.Update(orderToUpdate);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
     }
 }
--- RestWebApi/Repositories/Interfaces/IRepository.cs
+++ RestWebApi/Repositories/Interfaces/IRepository.cs
@@ -4,7 +4,15 @@ namespace Repositories.Interfaces
 {
     internal interface IRepository
     {
-        public List<Order> GetOrders();
+        public Task<List<Order>> GetOrders();
+        
+        public Task<Order> GetOrderById(int orderNumber);
+
+        public Task<bool> CreateOrder(Order orderToCreate);
+
+        public Task<bool> UpdateOrder(Order orderToUpdate);
+
+        public Task<bool> DeleteOrder(int orderNumberToDelete);
 
     }
 }
